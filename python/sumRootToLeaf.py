e005a09237f48dc31ec4888e743ca8ba34696bea
imisi99 <isongrichard234@gmail.com>
2026-06-03 23:56:44 +0100

    Sum root to leaf

---
+++ python/sumRootToLeaf.py
@@ -0,0 +1,9 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def sumNumbers(root: TreeNode | None) -> int:
+    return 0
