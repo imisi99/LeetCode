eeccb7ef86980864435b2609a4f1f9297ef45c81
imisi99 <isongrichard234@gmail.com>
2026-06-06 23:49:22 +0100

    fixup! Sum root to leaf

--- go/SumRootToLeaf/SumRootToLeaf.go
+++ go/SumRootToLeaf/SumRootToLeaf.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -7,5 +12,30 @@ type TreeNode struct {
 }
 
 func sumNumbers(root *TreeNode) int {
-	return 0
+	sum := 0
+	traverse(root, "", &sum)
+	return sum
+}
+
+func traverse(root *TreeNode, path string, sum *int) {
+	if root == nil {
+		return
+	}
+
+	path += fmt.Sprintf("%v", root.Val)
+	if root.Left == nil && root.Right == nil {
+		pathInt, _ := strconv.Atoi(path)
+		*sum += pathInt
+		return
+	}
+
+	traverse(root.Left, path, sum)
+	traverse(root.Right, path, sum)
+}
+
+func main() {
+	left := &TreeNode{Val: 2}
+	right := &TreeNode{Val: 3}
+	root := &TreeNode{Val: 1, Left: left, Right: right}
+	fmt.Println(sumNumbers(root))
 }
--- python/sumRootToLeaf.py
+++ python/sumRootToLeaf.py
@@ -6,4 +6,25 @@ class TreeNode:
 
 
 def sumNumbers(root: TreeNode | None) -> int:
-    return 0
+    sumPaths = [0]
+    traverse(root, "", sumPaths)
+    return sumPaths[0]
+
+
+def traverse(root: TreeNode | None, path: str, pathsSum: list[int]):
+    if root is None:
+        return
+
+    path += str(root.val)
+    if root.left is None and root.right is None:
+        pathsSum[0] += int(path)
+        return
+
+    traverse(root.left, path, pathsSum)
+    traverse(root.right, path, pathsSum)
+
+
+left = TreeNode(val=2)
+right = TreeNode(val=3)
+root = TreeNode(val=1, left=left, right=right)
+print(sumNumbers(root))
