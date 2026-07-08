da6bc618e15086cd9ca408c1f355721b3ce2b610
imisi99 <isongrichard234@gmail.com>
2026-07-08 23:46:49 +0100

    Compare Version

---
+++ go/CompareVersionNum/compareVersion.go
@@ -0,0 +1,9 @@
+package main
+
+func compareVersion(version1, version2 string) int {
+	return 0
+}
+
+func main() {
+	
+}
---
+++ python/compareVersion.py
@@ -0,0 +1,2 @@
+def compareVersion(version1: str, version2: str) -> int:
+    return 0
