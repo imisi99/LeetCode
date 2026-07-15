9d7b3f8eaf25b0a18e536aa0e2e64761c93e815b
imisi99 <isongrichard234@gmail.com>
2026-07-15 23:55:37 +0100

    Game of life

---
+++ go/GameOfLife/GameOfLife.go
@@ -0,0 +1,12 @@
+package main
+
+func gameOfLife(board [][]int) {
+
+}
+
+func main() {
+	board := [][]int{
+		{1, 0, 1},
+	}
+	gameOfLife(board)
+}
---
+++ python/gameOfLife.py
@@ -0,0 +1,9 @@
+def gameOfLife(board: list[list[int]]):
+    pass
+
+
+board = [
+    [0, 1, 0]
+]
+
+gameOfLife(board)
